16f7d347fdb117518a680f7ead80e8728982d05b
JosemyAB <josemitd@gmail.com>
2019-04-09 13:36:13 +0200

    Check if lazy module can load with auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {HomeComponent} from './home/home.component';
+import {AuthGuardService} from './auth/auth-guard.service';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
+  {path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule', canLoad: [AuthGuardService]},
   {path: 'shopping-list', component: ShoppingListComponent},
 ];
 
