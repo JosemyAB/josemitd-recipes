96b2652e5a6e38dbc7ee7104d6ff8c9ccb3d421d
JosemyAB <josemitd@gmail.com>
2019-04-09 13:58:01 +0200

    Load Services in core module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
-import {RecipeService} from './recipes/recipe.service';
-import {DataStorageService} from './common/data-storage.service';
-import {AuthService} from './auth/auth.service';
-import {AuthGuardService} from './auth/auth-guard.service';
 import {SharedModule} from './common/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {AuthModule} from './auth/auth.module';
@@ -27,7 +22,6 @@ import {CoreModule} from './core/core.module';
     SharedModule,
     CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,11 @@ import {HeaderComponent} from './header/header.component';
 import {HomeComponent} from './home/home.component';
 import {SharedModule} from '../common/shared.module';
 import {AppRoutingModule} from '../app-routing.module';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+import {RecipeService} from '../recipes/recipe.service';
+import {DataStorageService} from '../common/data-storage.service';
+import {AuthService} from '../auth/auth.service';
+import {AuthGuardService} from '../auth/auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,13 @@ import {AppRoutingModule} from '../app-routing.module';
   exports: [
     AppRoutingModule,
     HeaderComponent
-  ]
+  ],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    DataStorageService,
+    AuthService,
+    AuthGuardService]
 })
 export class CoreModule {
 
